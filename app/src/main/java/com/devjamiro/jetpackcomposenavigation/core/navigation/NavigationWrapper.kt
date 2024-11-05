047183d04508f127695a72115c67976bddb1bf0c
Jamiro <devjamiro@gmail.com>
2024-11-04 22:55:44 -0300

    feat: implement navigation and login screen

---
+++ app/src/main/java/com/devjamiro/jetpackcomposenavigation/core/navigation/NavigationWrapper.kt
@@ -0,0 +1,17 @@
+package com.devjamiro.jetpackcomposenavigation.core.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.devjamiro.jetpackcomposenavigation.LoginScreen
+
+@Composable
+fun NavigationWrapper() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Login) {
+        composable<Login> {
+            LoginScreen()
+        }
+    }
+}
---
+++ app/src/main/java/com/devjamiro/jetpackcomposenavigation/core/navigation/Screens.kt
@@ -0,0 +1,7 @@
+package com.devjamiro.jetpackcomposenavigation.core.navigation
+
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+object Login
